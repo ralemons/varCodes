96598854bd57872164a44699ec0e37934e75bde2
ralemons <46497943+ralemons@users.noreply.github.com>
2019-07-23 13:52:19 -0700

    Update directorySSIM.m
    Added most of the final functionality. It can now read a directory and move files if they are the same and includes a viewing function that has no purpose but to look at similar images yet.

--- imageRecognition/directorySSIM.m
+++ imageRecognition/directorySSIM.m
@@ -1,26 +1,78 @@
+%% Load directory and image information
 clear
 
+% load in .mat files with directory information. (better git integration)
+load('dirNames.mat');
+dirProps = dir(startDir);
 
-for ii = 1:5
-    fileName = ['C:\Users\rlemons\Documents\Pictures\Test Roses\r',num2str(ii),'.jpg'];
-    img{ii} = imresize(imread(fileName),[300 300]);
-    if size(img{ii},3) == 3
-        img{ii} = rgb2gray(img{ii});
+% remove all directories from dir listing
+N = size(dirProps,1);
+for ii = N:-1:1
+    if dirProps(ii).isdir == 1
+        dirProps(ii) = [];
+    end
+end
+N = size(dirProps,1); % find new size of the file list
+
+[imgs{1:N}] = deal(zeros(300)); % pre-allocate for thumbnail cell array
+for ii = 1:N
+    fileName = fullfile(dirProps(ii).folder,dirProps(ii).name);
+    imgs{ii} = imresize(imread(fileName),[300 300]);
+    if size(imgs{ii},3) == 3
+        imgs{ii} = rgb2gray(imgs{ii}); % ssim needs two dimentional data
     end
 end
 
-tst = repmat(img,1,10);
+%% Compare the images
+ssimMat = zeros(N);
+dupeFiles = zeros(N,1);
 
-N = length(tst);
 
-tmp = zeros(N);
-
-for zz = 1:5
-tic
-for jj = 1:N
-    for ii = jj+1:N
-        tmp(jj,ii) = slimSSIM(tst{jj},tst{ii});
+for jj = 1:N % Iterate through each image
+    for ii = jj+1:N % Compare only to images after it
+        ssimMat(jj,ii) = slimSSIM(imgs{jj},imgs{ii});
+        if ssimMat(jj,ii) == 1
+            dupeFiles(ii) = 1; % record if there is a truly identical image
+        end
     end
 end
-toc
+ssimMat = ssimMat + ssimMat';
+
+%% Display very close files for comparison
+
+
+
+for ii = 1:N
+    [vals,inds] = sort(ssimMat(ii,:));
+    trash = vals == 1 | vals == 0;
+    vals(trash) = [];
+    inds(trash) = [];
+    inds = flip(inds);
+    
+    imgsDisp{4} = 0;
+    
+    imgsDisp{1} = imread(fullfile(dirProps(ii).folder,dirProps(ii).name));
+    imgsDisp{1} = insertText(imgsDisp{1},...
+        [0 0;0 round(size(imgsDisp{1},1).*0.032.*2)],dirProps(ii).name,...
+        'AnchorPoint','LeftBottom',...
+        'FontSize',round(size(imgsDisp{1},1).*0.032));
+    for jj = 1:3
+        imgsDisp{jj+1} =...
+            imread(fullfile(dirProps(inds(jj)).folder,dirProps(inds(jj)).name));
+        imgsDisp{jj+1} = insertText(imgsDisp{jj+1},...
+            [0 0;0 round(size(imgsDisp{jj+1},1).*0.032.*2)],dirProps(inds(jj)).name,...
+            'AnchorPoint','LeftBottom',...
+            'FontSize',round(size(imgsDisp{jj+1},1).*0.032));
+    end
+    
+    montage(imgsDisp);
+    [~] = input('wait');
 end
+
+%% Move Identical Files
+for ii = 1:N
+    if dupeFiles(ii) == 1
+        movefile(fullfile(dirProps(ii).folder,dirProps(ii).name),moveDir)
+    end
+end
+
